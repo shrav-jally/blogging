a7a667c9f4597b3e472772fce7818a27c42b301f
GodishalaAshwith <ashwith7777@gmail.com>
2024-10-09 16:54:20 +0530

    Login&Register

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 import Post from "./Post";
 import { Routes, Route } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
   );
