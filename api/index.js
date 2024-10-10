0d51635a2aec82ed66eb818c1be1c0520f55cb89
GodishalaAshwith <ashwith7777@gmail.com>
2024-10-10 18:05:27 +0530

    Authentication Done

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ const app = express();
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const User = require("./models/User");
+const jwt = require("jsonwebtoken");
 
 const salt = bcrypt.genSaltSync(10);
+const secret = "dbaw7dp9awbdadjbaw97dawbdu21bn1";
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 mongoose.connect(
   "mongodb+srv://blogger:raBANyvLmkJe1hIR@cluster0.1lfeh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -27,6 +29,21 @@ app.post("/register", async (req, res) => {
     res.json(userDoc);
   } catch (err) {
     res.status(400).json(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    //logged in
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("wrong credentials");
   }
 });
 
